a0779947930029da004fb281be9dadaf64b6866a
elotwo <obiagwu94@gmail.com>
2023-09-14 23:35:24 +0100

    Debugging is fun

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *  * largest_number - returns the largest of 3 numbers
  *   * @a: first integer
@@ -7,6 +6,7 @@
  *     * @c: third integer
  *      * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -25,4 +25,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
