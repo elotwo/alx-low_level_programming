5fbe2abba371711df18795743366d0206763a93c
elotwo <obiagwu94@gmail.com>
2023-09-14 19:04:39 +0100

    3-print_remaining_days.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
+ * * main - Test function for positive or negative
+ * * Return: 0
  */
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
-	postitive_or_negative(i);
+	positive_or_negative(i);
 	return (0);
 }
+
