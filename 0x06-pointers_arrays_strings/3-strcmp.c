75d90e21388f53500fb9f17b65fec03695242ea2
elotwo <obiagwu94@gmail.com>
2023-09-22 17:09:11 +0100

    hard things

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-#include<string.h>
+#include "main.h"
 /**
- * _strcmp - A program for comparing strings
- * @s1: first varaible string
- * @s2: secound variable string
- * Return: (0)
+ *  * _strcmp - compare string values
+ *   * @s1: input value
+ *    * @s2: input value
+ *     *
+ *      * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (-15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	else if (s1 > s2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
+
