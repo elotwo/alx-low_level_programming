71e6fa13aea80e9da3e620fa88d428c37543354a
elotwo <obiagwu94@gmail.com>
2023-09-22 17:19:32 +0100

    doing hard things

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
+ *  * reverse_array - reverse array of integers
+ *   * @a: array
+ *    * @n: number of elements of array
+ *     *
+ *      * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	while (n > 0)
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
 	{
-		putchar(a[n]);
-		a--;
-		n--;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
-	putchar('\n');
 }
