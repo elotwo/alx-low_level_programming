503cdc06e3f21b8dabc5a7d4992427de290245e8
elotwo <obiagwu94@gmail.com>
2023-10-01 16:27:15 +0100

    Doing hard stuff

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * _strlen_recursion - A recursive string length program
+ * @s: pointer variabele s
+ * Return: (0)
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		return (0);
 	}
-	else{
-		s++;
-		return _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
