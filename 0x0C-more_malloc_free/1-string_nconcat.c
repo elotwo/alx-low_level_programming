11b1be221ae0b6b9c46b5127e2babf85f1541b8e
elotwo <obiagwu94@gmail.com>
2023-10-09 00:41:31 +0100

    holla

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	n = strlen(s2) + strlen(s1);
+	char *pum = (char *)malloc(n * sizeof(char));
+	if 
+
+	
