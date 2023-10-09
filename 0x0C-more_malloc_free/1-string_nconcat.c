2e34284f9cb61b45bb4086c5d19814290d1440cb
elotwo <obiagwu94@gmail.com>
2023-10-09 23:32:54 +0100

    me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,39 @@
-#include <stdio.h>
-#include <string.h>
+1-string_nconcat.c
+
 #include <stdlib.h>
+#include "main.h"
+
 /**
+ *  * *string_nconcat - concatenates n bytes of a string to another string
+ *   * @s1: string to append to
+ *    * @s2: string to concatenate from
+ *     * @n: number of bytes from s2 to concatenate to s1
+ *      *
+ *       * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int c = 0;
-	unsigned int m = 0;
-	 char *dem = malloc(c + n + 1);
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[c] != '\0')
-	{
-		c++;
-	}
-	while (s2[m] != '\0')
-	{
-		m++;
-	}
-	if (n > m)
-	{
-		n = m;
-	}
-	if (dem == NULL)
-	{
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	}
-	c = m = 0;
-
-	while (s1[c] != '\0')
+	while (i < len1)
 	{
-		dem[c] = s1[c];
-		c++;
+		s[i] = s1[i];
+		i++;
 	}
-
-	while (m < n)
-	{
-		dem[m] = s2[m];
-		c++;
-	       	m++;
-	}
-	dem[c] = '\0';
-	return (dem);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
-
