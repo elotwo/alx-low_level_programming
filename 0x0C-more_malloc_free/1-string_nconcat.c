6ba4d8fc3b5e787e8c3f03ffa66a5a3042b880fe
elotwo <obiagwu94@gmail.com>
2023-10-09 23:16:45 +0100

    me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /**
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	n = strlen(s2) + strlen(s1);
-	char *pum = (char *)malloc(n * sizeof(char));
-	if 
+	unsigned int c = 0;
+	unsigned int m = 0;
+	 char *dem = malloc(c + n + 1);
 
-	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[c] != '\0')
+	{
+		c++;
+	}
+	while (s2[m] != '\0')
+	{
+		m++;
+	}
+	if (n > m)
+	{
+		n = m;
+	}
+	if (dem == NULL)
+	{
+		return (NULL);
+	}
+	c = m = 0;
+
+	while (s1[c] != '\0')
+	{
+		dem[c] = s1[c];
+		c++;
+	}
+
+	while (m < n)
+	{
+		dem[m] = s2[m];
+		c++;
+	       	m++;
+	}
+	dem[c] = '\0';
+	return (dem);
+}
+
