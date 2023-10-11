e1620cb55ad3c8b140bdac8b49a94bf355ffd8e8
elotwo <obiagwu94@gmail.com>
2023-10-11 01:01:40 +0100

    my job

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
 /**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: variable d
+ * @name: variable name
+ * @age: variable age
+ * @owner: variable owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = strdup(name);
+	d->name = name;
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = owner;
 }
 
