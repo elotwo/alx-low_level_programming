eac550ee377cd796363a2479200337898e510204
elotwo <obiagwu94@gmail.com>
2023-10-11 14:33:05 +0100

    forever yong

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,10 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 /**
+ * _strlen - A program that print length of a string
+ * @s: variable s
+ * Return: (0)
  */
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -17,11 +21,16 @@ int _strlen(char *s)
 #include <string.h>
 #include <stdlib.h>
 /**
+ * _strcpy -Program that copy strings
+ * @dest: Variable destination for copy strings
+ * @src: variable source for destinatination strings
+ * Return: (0)
  */
 char *_strcpy(char *dest, char *src)
 {
 	int len;
 	int i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
@@ -37,12 +46,18 @@ char *_strcpy(char *dest, char *src)
 #include <stdio.h>
 #include "dog.h"
 /**
+ * new_dog - A program store variable name and owner
+ * @name: varaible name
+ * @age: variable age
+ * @owner: variable owner
+ * Return: (0)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	 int lent1;
 	 int lent2;
-	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
+
+	 dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 	{
@@ -54,14 +69,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->name == NULL)
 	{
 		free(dog);
-		return(NULL);
+		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (lent2 + 2));
 	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
-		return(NULL);
+		return (NULL);
 	}
 	dog->age = age;
 	_strcpy(dog->name, name);
