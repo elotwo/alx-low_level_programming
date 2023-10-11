4668523fb56a555e3ec618ba7386bce52752eb62
elotwo <obiagwu94@gmail.com>
2023-10-11 23:00:04 +0100

    me

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#define MSG "2-main.c"
 /**
  * main - A progrm that print it's name
  * Return: (0)
  */
 int main(void)
 {
-	printf("%s \n", MSG);
+	printf("%s \n", _FILE_);
 	return (0);
 }
