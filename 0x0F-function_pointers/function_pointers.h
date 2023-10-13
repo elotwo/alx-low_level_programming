60fe1e1cf5c6c7ae1cdb6baccaa1df298057be67
elotwo <obiagwu94@gmail.com>
2023-10-13 23:22:56 +0100

    hard things

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef POINTERS_H
 #define POINTERS_H
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
