6d4e064b08cf23e88404815dc167dc7b97c556fd
elotwo <obiagwu94@gmail.com>
2023-10-19 22:26:57 +0100

    me

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_list - A function that print all element of a structurw
+ * list
+ * @h: variable arguiment
+ * Return: (0)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
