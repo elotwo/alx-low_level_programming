7978f58fd32579f5f34675ff6144d41538bd858d
elotwo <obiagwu94@gmail.com>
2023-10-26 23:24:06 +0100

    still here

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include "main.h"
 /**
+ * binary_to_uint - A function that convert decimals to
+ * binary
+ * @b: variable input where binary are tp be entered;
+ * Return: (0)
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -12,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 	return (0);
 	for (i = 0; i < length; i++)
-	{ 
+	{
 		if (b[i] == '0')
 		{
 			result = (result << 1);
@@ -28,4 +32,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (result);
 }
-	
