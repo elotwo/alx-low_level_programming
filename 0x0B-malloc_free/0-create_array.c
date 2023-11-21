587feb7ccfd7d73c8cc3e63ebb59c6e4fc983fc0
elotwo <obiagwu94@gmail.com>
2023-11-21 23:21:50 +0100

    me

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - A program that print chararcters
+ * @size: variable size
+ * @c: variable character
+ * Return: (0)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = (char *)malloc(sizeof(char) * size);
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
