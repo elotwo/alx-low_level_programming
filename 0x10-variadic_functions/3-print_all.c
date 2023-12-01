bdf12047a08a701ee2b49c94928965717ba96d22
elotwo <obiagwu94@gmail.com>
2023-12-01 13:18:38 +0100

    me

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -37,7 +37,7 @@ void print_all(const char * const format, ...)
 		i++;
 	}
 }
-printf("\n");
+printf('\n');
 va_end(list);
 }
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,4 @@
-#ifndef _VARIADIC_H_
+#ifndef _VARIADIC_H
 #define _VARIADIC_H
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
