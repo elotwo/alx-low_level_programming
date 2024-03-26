f35a421f527bae431307d384e02b346bdb0b0814
elotwo <obiagwu94@gmail.com>
2024-03-26 04:30:18 -0700

    commit myself

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -4,20 +4,21 @@
  *   * @s: input
  *    * @accept: input
  *     * Return: Always 0 (Success)
- **/
+ *      */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-	
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-			s == accept[k];														return (s);
+		int k;
+
+			while (*s)
+					{
+								for (k = 0; accept[k]; k++)
+											{
+														if (*s == accept[k])
+																	return (s);
 																}
 									s++;
 										}
 
-			return (0);
+			return ('\0');
 }
 
