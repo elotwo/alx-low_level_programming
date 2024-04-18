04d08f8fd2a34d2b06b873f39165589b0ef82855
elotwo <obiagwu94@gmail.com>
2024-04-18 13:34:34 +0100

    na me o

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return NULL;
+	}
+	table->array = (hash_node_t**)malloc(size * sizeof (hash_node_t *));
+	if (table->array== NULL)
+	{
+		free(table);
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	table->size = size;
+	return table;
+}
