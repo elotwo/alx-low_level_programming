23f78543735e91112b3ed78ef2ebb53886ff84a3
elotwo <obiagwu94@gmail.com>
2024-04-18 19:15:06 +0100

    dem

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,26 +4,27 @@
 /**
  * hash_table_create - A Function that a hash table
  * @size: variable size of for the hash table
- * return: (0)sucessfull
+ * Return: (0)sucessfull
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	table->array = (hash_node_t**)malloc(size * sizeof (hash_node_t *));
-	if (table->array== NULL)
+	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
 		free(table);
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
 	table->size = size;
-	return table;
+	return (table);
 }
