8942a38aae062efce567d52cebf9b0930ae3b5a0
elotwo <obiagwu94@gmail.com>
2024-04-19 00:22:16 +0100

    me

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,4 +6,11 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+	unsigned long int index = 0;
+	while (*key)
+	{
+		index += *key;
+		key++;
+	}
+	return index %size;
+}
