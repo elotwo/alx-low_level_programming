b37426bbbdbe5d00919340f4ff77fb9c52a00a61
elotwo <obiagwu94@gmail.com>
2024-07-15 23:41:03 +0100

    me

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+/*
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	double i = sqrt(size_t);
+	for (n 
